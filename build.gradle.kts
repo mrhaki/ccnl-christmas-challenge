72f4028dcc5a6b5e27a668e47062941e14e08517
Hubert A. Klein Ikkink <h.kleinikkink@gmail.com>
2021-12-01 17:29:13 +0100

    Replace Maven with Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    id("io.quarkus")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:2.5.1.Final"))
+
+    implementation("io.quarkus:quarkus-spring-web")
+    implementation("io.quarkus:quarkus-spring-boot-properties")
+    implementation("io.quarkus:quarkus-arc")
+    implementation("org.projectlombok:lombok:1.18.22")
+
+    testImplementation("io.quarkus:quarkus-junit5-mockito")
+    testImplementation("org.mockito:mockito-core:4.0.0")
+    testImplementation("io.rest-assured:rest-assured:4.4.0")
+}
+
+group = "com.mrhaki"
+version = "0.1.0-SNAPSHOT"
+description = "ccnl-christmas-challenge"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("io.quarkus") version "2.5.1.Final"
+    }
+}
+
+rootProject.name = "ccnl-christmas-challenge"
